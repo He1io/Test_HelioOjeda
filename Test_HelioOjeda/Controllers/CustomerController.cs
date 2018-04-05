aea1e0393c9de37bfe3540de74d1e56634042b75
He1io <helio.ojeda.reyes@gmail.com>
2018-04-05 12:01:03 +0100

    Finished simple CRUD operations
    - Finished the "Get/Post/Put/Delete" implementation in the Controller.
    
    - Finished the "Get/Create/Update/Delete" implementation in the Service.

--- Test_HelioOjeda/Controllers/CustomerController.cs
+++ Test_HelioOjeda/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
-using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Test_HelioOjeda.Models;
 using Test_HelioOjeda.Services;
@@ -13,51 +14,78 @@ namespace Test_HelioOjeda.Controllers
     {
         // GET: api/Customer
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ArrayList Get()
         {
-            return new string[] { "Customer1", "Customer2" };
+            CustomerService customerService = new CustomerService();
+            return customerService.getCustomers();
         }
 
         // GET: api/Customer/id
         [HttpGet("{id}", Name = "Get")]
         public Customer Get(int id)
         {
-            Customer customer = new Customer();
-            customer.Id = id;
-            customer.Name = "Helio";
-            customer.Surname = "Ojeda";
-            customer.PhotoURL = "www.prueba.com";
-
-            return customer;
+            CustomerService customerService = new CustomerService();
+            return customerService.getCustomer(id);
         }
-        
-        // POST: api/Customer
-        // I have to pass the Customer from the request in JSON (ID is "autoincrement" so I don't need to pass it)
-        /* Example:
+
+
+        /* In POST and PUT I have to pass the Customer from the client request in JSON (ID is "autoincrement" so I don't need to pass it)
+         * Example:
          * {
          * "name":"Helio"
          * "surname":"Ojeda"
          * "photoUrl":"www.prueba.com"
          * }
          */
+
+        // POST: api/Customer
         [HttpPost]
-        public void Post([FromBody]Customer customer)
+        public HttpResponseMessage Post([FromBody]Customer customer)
         {
             CustomerService customerService = new CustomerService();
-
             customerService.createCustomer(customer);
+
+            //Return a "Created" status code
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.StatusCode = HttpStatusCode.Created;
+            return response;
         }
         
         // PUT: api/Customer/id
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public HttpResponseMessage Put(int id, [FromBody]Customer updatedCustomer)
         {
+            CustomerService customerService = new CustomerService();
+            bool customerUpdated = false;
+            customerUpdated = customerService.updateCustomer(id, updatedCustomer);
+
+            if (customerUpdated)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NoContent);
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
         }
         
         // DELETE: api/ApiWithActions/id
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public HttpResponseMessage Delete(int id)
         {
+            CustomerService customerService = new CustomerService();
+            bool customerDeleted = false;
+            customerDeleted = customerService.deleteCustomer(id);
+
+            if (customerDeleted)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NoContent);
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
         }
     }
 }
--- Test_HelioOjeda/Services/CustomerService.cs
+++ Test_HelioOjeda/Services/CustomerService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -36,8 +37,103 @@ namespace Test_HelioOjeda.Services
                "VALUES ('" + customerToSave.Name + "', '" + customerToSave.Surname + "', '" + customerToSave.PhotoURL + "')";
 
             MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(sql, connection);
-
             command.ExecuteNonQuery();
+        }
+
+        //READ ALL CUSTOMERS
+        public ArrayList getCustomers()
+        {
+            ArrayList customers = new ArrayList();
+
+            string sql = "SELECT * FROM customers";
+
+            MySql.Data.MySqlClient.MySqlDataReader mySQLReader = null;
+            MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(sql, connection);
+            mySQLReader = command.ExecuteReader();
+
+            //While there were customers, keep reading them
+            while (mySQLReader.Read())
+            {
+                Customer customer = new Customer();
+                customer.Id = mySQLReader.GetInt16(0);
+                customer.Name = mySQLReader.GetString(1);
+                customer.Surname = mySQLReader.GetString(2);
+                customer.PhotoURL = mySQLReader.GetString(3);
+                customers.Add(customer);
+            }
+
+            return customers;
+        }
+
+        //READ CUSTOMER BY ID
+        public Customer getCustomer(int id)
+        {
+            Customer customer = new Customer();
+
+            string sql = "SELECT * FROM customers WHERE id = " + id;
+
+            MySql.Data.MySqlClient.MySqlDataReader mySQLReader = null;
+            MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(sql, connection);
+            mySQLReader = command.ExecuteReader();
+
+            //If the customer exists, read him
+            if (mySQLReader.Read())
+            {
+                customer.Id = mySQLReader.GetInt16(0);
+                customer.Name = mySQLReader.GetString(1);
+                customer.Surname = mySQLReader.GetString(2);
+                customer.PhotoURL = mySQLReader.GetString(3);
+                return customer;
+            }
+
+            else return null;
+        }
+
+        //UPDATE CUSTOMER
+        public bool updateCustomer(int id, Customer updatedCustomer)
+        {
+            string sql = "SELECT * FROM customers WHERE id = " + id;
+
+            MySql.Data.MySqlClient.MySqlDataReader mySQLReader = null;           
+            MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(sql, connection);
+            mySQLReader = command.ExecuteReader();
+
+            //If the customer exists, update him
+            if (mySQLReader.Read())
+            {
+                string updateSql = "UPDATE customers SET name = '" + updatedCustomer.Name + "', surname = '" + updatedCustomer.Surname
+                + "', photoUrl = '" + updatedCustomer.PhotoURL + "'  WHERE id = " + id;
+
+                mySQLReader.Close();
+                command = new MySql.Data.MySqlClient.MySqlCommand(updateSql, connection);
+                command.ExecuteNonQuery();
+                return true;
+            }
+
+            else return false;
+        }
+
+        //DELETE CUSTOMER BY ID
+        public bool deleteCustomer(int id)
+        {
+            string sql = "SELECT * FROM customers WHERE id = " + id;
+
+            MySql.Data.MySqlClient.MySqlDataReader mySQLReader = null;
+            MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(sql, connection);
+            mySQLReader = command.ExecuteReader();
+
+            //If the customer exists, delete him
+            if (mySQLReader.Read())
+            {
+                string deleteSql = "DELETE FROM customers WHERE id = " + id;
+
+                mySQLReader.Close();
+                command = new MySql.Data.MySqlClient.MySqlCommand(deleteSql, connection);
+                command.ExecuteNonQuery();
+                return true;
+            }
+
+            else return false;
         }
     }
 }
